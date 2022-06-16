5d3e4f74bc09b4441b1e991dab4d5162150b7859
hsuan619 <b11023055@gemail.yuntech.edu.tw>
2022-06-16 15:10:26 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Ex1301
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// 設計工具所需的變數。
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// 清除任何使用中的資源。
+      /// </summary>
+      /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form 設計工具產生的程式碼
+
+      /// <summary>
+      /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+      /// 這個方法的內容。
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rabFemale = new System.Windows.Forms.RadioButton();
+            this.rabMale = new System.Windows.Forms.RadioButton();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rabFemale);
+            this.groupBox1.Controls.Add(this.rabMale);
+            this.groupBox1.Location = new System.Drawing.Point(12, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 60);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "性別";
+            // 
+            // rabFemale
+            // 
+            this.rabFemale.AutoSize = true;
+            this.rabFemale.Location = new System.Drawing.Point(120, 31);
+            this.rabFemale.Name = "rabFemale";
+            this.rabFemale.Size = new System.Drawing.Size(53, 29);
+            this.rabFemale.TabIndex = 1;
+            this.rabFemale.Text = "女";
+            this.rabFemale.UseVisualStyleBackColor = true;
+            // 
+            // rabMale
+            // 
+            this.rabMale.AutoSize = true;
+            this.rabMale.Location = new System.Drawing.Point(25, 31);
+            this.rabMale.Name = "rabMale";
+            this.rabMale.Size = new System.Drawing.Size(53, 29);
+            this.rabMale.TabIndex = 0;
+            this.rabMale.Text = "男";
+            this.rabMale.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(522, 355);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(77, 36);
+            this.btnConfirm.TabIndex = 12;
+            this.btnConfirm.Text = "確認";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "學號:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "問卷調查";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox5);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Location = new System.Drawing.Point(20, 298);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(237, 250);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "興趣";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(17, 215);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(94, 29);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "逛賣場";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(17, 169);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(94, 29);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "聽音樂";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 29);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "跑步";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 123);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 29);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "爬山";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(17, 77);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 29);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "看書";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(264, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "專長:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(617, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 36);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "結束";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(548, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "學生基本資料";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 34);
+            this.textBox1.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "重填";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "20歲以下",
+            "21~40 歲",
+            "41~60 歲",
+            "61以上"});
+            this.comboBox2.Location = new System.Drawing.Point(119, 217);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 33);
+            this.comboBox2.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "年齡：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "數學",
+            "英文",
+            "程式設計",
+            "系統分析"});
+            this.listBox1.Location = new System.Drawing.Point(333, 309);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 104);
+            this.listBox1.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(548, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(554, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 25);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(522, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 36);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "清除資料";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 601);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("微軟正黑體", 11.8209F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "學生基本資料輸入";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rabFemale;
+        private System.Windows.Forms.RadioButton rabMale;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1301
+{
+   public partial class Form1 : Form
+   {
+        private int count = 1;
+
+        public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void btnConfirm_Click(object sender, EventArgs e)
+      {
+         //建立一個處理文字方塊字串的陣列
+         String[] temps = new String[5];
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("請輸入學號", "錯誤");
+                button3_Click(sender, e);
+                label7.Text = "";
+            }
+            else
+            {
+                temps[0] = $"學號：{textBox1.Text}";
+            }
+
+            if (rabMale.Checked)
+            {
+                temps[1] = $"性別：{rabMale.Text}";
+            }
+            else if (rabFemale.Checked)
+            {
+                temps[1] = $"性別：{rabFemale.Text}";
+            }
+            else
+            {
+                MessageBox.Show("請選擇性別", "錯誤");
+                button3_Click(sender, e);
+                label7.Visible = false;
+            }
+            if (listBox1.SelectedIndex == 0)
+            {
+                temps[4] += $"專長：{listBox1.Text}";
+            }
+            
+            if (listBox1.SelectedIndex == 1)
+            {
+                temps[4] += $"專長：{listBox1.Text}";
+            }
+            if(listBox1.SelectedIndex == 2)
+            {
+                temps[4]+= $"專長：{listBox1.Text}";
+            }
+            if (listBox1.SelectedIndex == 3)
+            {
+                temps[4] += $"專長：{listBox1.Text}";
+            }
+            if (checkBox1.Checked)
+            {
+                temps[3] += $"興趣：{checkBox1.Text} ";
+            }
+            if (checkBox2.Checked)
+            {
+                temps[3] += $"興趣：{checkBox2.Text} ";
+            }
+            if (checkBox3.Checked)
+            {
+                temps[3] += $"興趣：{checkBox3.Text} ";
+            }
+            if (checkBox4.Checked)
+            {
+                temps[3] += $"興趣：{checkBox4.Text} ";
+            }
+            if (checkBox5.Checked)
+            {
+                temps[3] += $"興趣：{checkBox5.Text} ";
+            }
+            if (comboBox2.SelectedIndex == 0)
+            {
+                temps[2] = $"年齡：{comboBox2.Text}";
+            }
+            else if(comboBox2.SelectedIndex == 1)
+            {
+                temps[2] = $"年齡：{comboBox2.Text}";
+            }
+            else if (comboBox2.SelectedIndex == 2)
+            {
+                temps[2] = $"年齡：{comboBox2.Text}";
+            }
+            else if(comboBox2.SelectedIndex == 3)
+            {
+                temps[2] = $"年齡：{comboBox2.Text}";
+            }
+            else
+            {
+                MessageBox.Show("請選擇年齡","錯誤");
+                button3_Click(sender, e);
+                label7.Visible = false;
+            }
+
+
+            //取得陣列內容放入文字方塊
+            label7.Text = temps[0]+"\n"+ temps[1] + "\n" + temps[2] + "\n" + temps[3] + "\n" + temps[4];
+            label7.Visible = true;
+      }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.ClearSelected();
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            comboBox2.SelectedIndex = -1;
+            rabMale.Checked = false;
+            label7.Text = "";
+            textBox1.Text = "";
+        }
+    }
+}
